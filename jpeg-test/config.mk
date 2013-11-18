309e8898fa471f85179504407bdcab9b49627147
Patrick Wood <patrickhwood@gmail.com>
2013-11-18 18:40:16 -0500

    added slideshow.c

--- jpeg-test/config.mk
+++ jpeg-test/config.mk
@@ -1,6 +1,6 @@
 TARGET = jpeg-test
 LIB = libjpeg.a
-LIBSRC = jpeg.c decode.c show.c ../common/disp.c ../common/ve.c
-SRC = main.c jpeg.c decode.c show.c ../common/disp.c ../common/ve.c
+LIBSRC = jpeg.c decode.c show.c slideshow.c ../common/disp.c ../common/ve.c
+SRC = main.c jpeg.c decode.c show.c slideshow.c ../common/disp.c ../common/ve.c
 CFLAGS = -Wall -Wextra -O2
 LDFLAGS = 

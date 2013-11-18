1aeb8c1b82b5c2a7d2484093c2c1c7e7f9aa020e
Patrick Wood <patrickhwood@gmail.com>
2013-11-18 18:30:38 -0500

    Added libjpeg.a target

--- jpeg-test/config.mk
+++ jpeg-test/config.mk
@@ -1,4 +1,6 @@
 TARGET = jpeg-test
+LIB = libjpeg.a
+LIBSRC = jpeg.c decode.c show.c ../common/disp.c ../common/ve.c
 SRC = main.c jpeg.c decode.c show.c ../common/disp.c ../common/ve.c
 CFLAGS = -Wall -Wextra -O2
 LDFLAGS = 
